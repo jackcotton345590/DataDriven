c17da7940037883cc0f28e1fdc32a8629ebfbf15
jackcotton345590 <44164652+jackcotton345590@users.noreply.github.com>
2019-01-30 23:00:30 +0000

    Create Header.h

---
+++ App/Header.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+void menu();  //menu function that displays all options in the main.cpp
+int calculation(char count); 
+void displayResult (); 
