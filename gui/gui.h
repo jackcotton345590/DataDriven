984893ebde49852ad504c90e906aa506ffb4a067
jackcotton345590 <44164652+jackcotton345590@users.noreply.github.com>
2019-01-30 22:59:07 +0000

    Create gui.h
    yes

---
+++ gui/gui.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+		ofRectangle tweetbtn;
+		ofImage TwitterSet;
+		ofRectangle btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10;
+		
+};
